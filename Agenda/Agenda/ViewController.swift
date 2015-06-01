b5440f6ff28fb7430154caa7a88dd339489528c4
Bruno Omella Mainieri <bruno.omella@hotmail.com>
2015-06-01 14:57:01 -0300

    Commit inicial

---
+++ Agenda/Agenda/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Agenda
+//
+//  Created by Bruno Omella Mainieri on 6/1/15.
+//  Copyright (c) 2015 Melhor Grupo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
